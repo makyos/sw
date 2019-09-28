e6176d2fa69acc9df73b907e3db92df55dafae97
makyos <makyos@gmail.com>
2019-09-28 22:34:38 +0900

    1

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+
 fn main() {
 
+	//// KEYBORD event from STDIO at new thread
     let stdin = stdin();
     let (tx, rx) = channel();
     thread::spawn(move || {
@@ -24,29 +26,32 @@ fn main() {
     });
 
     let mut stdout = stdout().into_raw_mode().unwrap();
-    let last_time = Local::now();
-	let _ = write!(stdout, "{}\r\n", last_time);
+    let start: chrono::DateTime<chrono::Local> = Local::now();
+
+	let _ = write!(stdout, "[spc]: split, [q]: stop & quit\r\n");
+	let _ = write!(stdout, "{} START!\r\n", start);
 
     loop {
-        let current_time = Local::now();
-        let _ = write!(stdout, "{}\r", current_time.to_string());
+
+        stdout.flush().unwrap();
+        let _ = write!(stdout, "{}\r", Local::now().to_string());
+
+		//// KEYBORD event hundle
         if let Ok(evt) = rx.recv_timeout(Duration::from_millis(100)) {
             match evt {
-                Event::Key(Key::Ctrl('c')) => {
-					let dur = current_time - last_time;
-                    let _ = write!(stdout, "{} duration {} sec \r\n", current_time.to_string(), dur.num_seconds());
+                Event::Key(Key::Char('q')) | Event::Key(Key::Ctrl('c')) => {
+                    let _ = write!(stdout, "{} GOAL! {} sec \r\n", Local::now().to_string(), (Local::now() - start).num_seconds());
                     return;
                 }
-                Event::Key(Key::Char(c)) => {
-                    if c == ' ' {
-						let dur = current_time - last_time;
-                        let _ = write!(stdout, "{} duration {} sec \r\n", current_time.to_string(), dur.num_seconds());
-                    }
+                Event::Key(Key::Char(' ')) => {
+                    let _ = write!(stdout, "{} duration {} sec \r\n", Local::now().to_string(), (Local::now() - start).num_seconds());
                 }
+				Event::Key(Key::Ctrl('n')) => {
+				}
                 _ => {}
             }
         }
-        stdout.flush().unwrap();
+
     }
 
 }
--- sw.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-	echo "$(tput cuu1)$(tput dl1)"
-	sleep 0.1
-	echo -n "$(date)"
-done
-
