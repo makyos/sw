ae6b00ef353bce4ac901be544e21209e65004b48
makyos <makyos@gmail.com>
2020-05-12 13:57:56 +0900

    fit TTY size

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,18 @@ use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-
 fn dur(start: &chrono::DateTime<chrono::Local>) -> String {
     let dur = Local::now() - *start;
-    return format!("+{:>02}:{:>02}:{:>02}({})",
-                   dur.num_hours()   % 24,
-                   dur.num_minutes() % 60,
-                   dur.num_seconds() % 60,
-                   dur.num_seconds());
+    return format!(
+        "+{:>02}:{:>02}:{:>02}({})",
+        dur.num_hours() % 24,
+        dur.num_minutes() % 60,
+        dur.num_seconds() % 60,
+        dur.num_seconds()
+    );
 }
 
 fn main() {
-
     //// KEYBORD event from STDIO at new thread
     let stdin = stdin();
     let (tx, rx) = channel();
@@ -41,12 +41,19 @@ fn main() {
     write!(stdout, "\n").unwrap();
 
     loop {
+        let (cols, _) = termion::terminal_size().unwrap();
+        write!(
+            stdout,
+            "{:.1$}\r",
+            format!("{} {}", Local::now().to_string(), dur(&start)),
+            cols as usize
+        )
+        .unwrap();
 
         stdout.flush().unwrap();
-        write!(stdout, "{} {}\r", Local::now().to_string(), dur(&start)).unwrap();
 
         //// KEYBORD event hundle
-        if let Ok(evt) = rx.recv_timeout(Duration::from_millis(10)) {
+        if let Ok(evt) = rx.recv_timeout(Duration::from_millis(16)) {
             match evt {
                 Event::Key(Key::Char('q')) | Event::Key(Key::Ctrl('c')) => {
                     write!(stdout, "\n").unwrap();
@@ -57,8 +64,5 @@ fn main() {
                 }
             }
         }
-
     }
-
 }
-
